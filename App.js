94a2567c00bc1150cf97546459a4b2711fe17b61
Jonathan Moseley <jryanmoseley@gmail.com>
2023-01-07 18:04:01 -0500

    playing with dimensions and orientation

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  Image,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
+
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
   console.log("Hello from my app");
+
+  console.log(useDimensions());
+  console.log(useDeviceOrientation());
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <Button
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
       <Text>Hello - react native is cool</Text>
+      <TouchableHighlight onPress={() => console.log("image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        ></Image>
+      </TouchableHighlight>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "darkgrey",
     alignItems: "center",
     justifyContent: "center",
   },
