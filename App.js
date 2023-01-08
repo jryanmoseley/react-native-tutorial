b7dbf0bb34492541ad22972f7d3161efb410168c
Jonathan Moseley <jryanmoseley@gmail.com>
2023-01-07 19:14:34 -0500

    welcome and view image screens

--- App.js
+++ App.js
@@ -1,36 +1,17 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 
 import {
   useDimensions,
   useDeviceOrientation,
 } from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   console.log("Hello from my app");
   console.log(useDimensions());
   console.log(useDeviceOrientation());
 
-  return (
-    <View style={styles.container}>
-      <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
-      />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "red", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "grey", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "green", width: 100, height: 100 }} />
-    </View>
-  );
+  return <WelcomeScreen />;
+  //return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row", // horizontal
-    flexWrap: "wrap", // wrap
-    backgroundColor: "darkgrey",
-    justifyContent: "center", // main axis
-    alignItems: "center", // secondary axis.  detemines alignment of content within each line
-    alignContent: "center",
-  },
-});
